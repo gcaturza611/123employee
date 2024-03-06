67e69ca6e8236e0946dc0521515edaa926a49491
gcaturza611 <gcaturza@611global.com>
2024-03-06 21:59:17 +0800

    marchsix

---
+++ assets/js/flip.js
@@ -0,0 +1,13 @@
+$("#owl-example").owlCarousel({
+  navigation: false, // Show next and prev buttons
+  pagination: false,
+
+  // "singleItem:true" is a shortcut for:
+  items: 4,
+  autoPlay: 1000,
+  stopOnHover: true,
+  // itemsDesktop : false,
+  // itemsDesktopSmall : false,
+  // itemsTablet: false,
+  // itemsMobile : false
+});
